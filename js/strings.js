d81e8b29ef9d7e61499409e5292c677e8a8b90b9
Joao Messi <joao.messi@movemais.com>
2020-01-17 14:17:30 -0300

    Adicionando arquivo de strings JS

---
+++ js/strings.js
@@ -0,0 +1,6 @@
+var strings = {
+	logo: "joao.lutz",
+	email: "joao.messilutz@gmail.com",
+	mailto: "mailto:joao.messilutz@gmail.com",
+	footer: "João Lutz &copy; 2020 &minus; Todos os direitos reservados"
+};
