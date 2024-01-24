2e3b1ed8ad905acec945defe207513db3a937082
João Gabriel Messi Lutz <tibepc@gmail.com>
2024-01-24 18:33:28 -0300

    atualização para 2024 e melhorias

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,22 @@
 function createMenu(current) {
 
-	var itens = [	
-	{nome: 'Home', href: 'index.html'},
-	{nome: 'Projetos', href: 'projetos.html'},	
-	{nome: 'Artigos', href: 'artigos.html'},
-	{nome: 'Parceiros', href: 'parceiros.html'},	                   
-	{nome: 'Contato', href: 'contato.html'}];
+	const items = [	
+		{nome: 'Home', href: 'index.html'},
+		{nome: 'Projetos', href: 'projetos.html'},	
+		{nome: 'Artigos', href: 'artigos.html'},
+		{nome: 'Parceiros', href: 'parceiros.html'},	                   
+		{nome: 'Contato', href: 'contato.html'}
+	];
 	
-	var ul = document.getElementById('menu');
+	const ul = document.getElementById('menu');
 	
-	for (var i in itens) {
-		var li = document.createElement("li"), a = document.createElement("a");
-		if(itens[i].nome == current) {
+	for (let i in items) {
+		let li = document.createElement("li"), a = document.createElement("a");
+		if(items[i].nome == current) {
 			li.classList.add("current");
 		}
-		a.innerHTML = itens[i].nome;
-		a.href = itens[i].href;
+		a.innerHTML = items[i].nome;
+		a.href = items[i].href;
 		li.appendChild(a);
 		ul.appendChild(li);
 	}
